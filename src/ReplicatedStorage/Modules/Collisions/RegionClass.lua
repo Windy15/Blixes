b2403b7b9c19ff6423ee332690706d7c5ec50af7
Windy15 <79671750+Windy15@users.noreply.github.com>
2024-05-04 19:16:03 -0400

    Optimized Region Class and IsPointInRegion method for regions with RegionType "Part"

--- src/ReplicatedStorage/Modules/Collisions/RegionClass.lua
+++ src/ReplicatedStorage/Modules/Collisions/RegionClass.lua
@@ -1,21 +1,40 @@
-local Region = {
-	CFrame = CFrame.identity,
-	Size = Vector3.zero,
-	RegionType = "Box"
-}
+local Region = {}
 Region.__index = Region
 
-function Region.new(new)
+function Region.new(regionType, cframeOrPart, size)
+	local new = {
+		RegionType = regionType,
+	}
+	if typeof(cframeOrPart) == "CFrame" then
+		new.CFrame = cframeOrPart
+		new.Size = size
+	else
+		new.Part = cframeOrPart
+	end
 	return setmetatable(new, Region)
 end
 
-function Region:PointInRegion(point)
+local function pointInPart(point, part)
+	if (point - part.Position).Magnitude < 0.05 then return true end
+
+	local params = RaycastParams.new()
+	params.FilterType = Enum.RaycastFilterType.Include
+	params.BruteForceAllSlow = true
+	params.FilterDescendantsInstances = {part}
+
+	local vectorToCenter = (part.Position - point).Unit * part.Size.Magnitude * 2
+	return workspace:Raycast(point - vectorToCenter, vectorToCenter, params) ~= nil
+end
+
+local function invalidRegionType(region)
+	error(`'{region.RegionType}' is not a valid region type`)
+end
+
+function Region:IsPointInRegion(point)
 	if self.RegionType == "Box" then
 		local newPoint = self.CFrame:PointToObjectSpace(point)
-
 		local TopCorner = self.Size / 2
 		local BottomCorner = -TopCorner
-
 		return
 			newPoint.X >= BottomCorner.X and
 			newPoint.X <= TopCorner.X and
@@ -23,57 +42,36 @@ function Region:PointInRegion(point)
 			newPoint.Y <= TopCorner.Y and
 			newPoint.Z >= BottomCorner.Z and
 			newPoint.Z <= TopCorner.Z
-
 	elseif self.RegionType == "Sphere" then
 		return (point - self.CFrame.Position).Magnitude <= self.Size
-	elseif self.RegionType == "Instance" then
-		local collisionPoint = Instance.new("Part")
-		collisionPoint.Position = point
-		collisionPoint.Size = Vector3.new(0.05, 0.05, 0.05)
-		collisionPoint.Anchored = true
-		collisionPoint.Transparency = 1
-		collisionPoint.CanCollide = false
-		collisionPoint.CanQuery = false
-		collisionPoint.Parent = workspace
-
-		local params = OverlapParams.new()
-		params.FilterType = Enum.RaycastFilterType.Include
-		params.FilterDescendantsInstances = {collisionPoint}
-		params.BruteForceAllSlow = true
-
-		local pointFound = table.find(workspace:GetPartsInPart(self.Instance, params))
-
-		collisionPoint:Destroy()
-
-		return pointFound and true
+	elseif self.RegionType == "Part" then
+		return pointInPart(point, self.Part)
 	else
-		error("Invalid Region Type")
+		invalidRegionType(self)
 	end
 end
 
 function Region:GetTouchingParts(params)
 	params = params or OverlapParams.new()
-	
+
 	if self.RegionType == "Box" then
 		return workspace:GetPartBoundsInBox(self.CFrame, self.Size, params)
 	elseif self.RegionType == "Sphere" then
 		return workspace:GetPartBoundsInRadius(self.CFrame, self.Size, params)
-	elseif self.RegionType == "Instance" then
-		return workspace:GetPartsInPart(self.Instance, params)
+	elseif self.RegionType == "Part" then
+		return workspace:GetPartsInPart(self.Part, params)
 	else
-		error("Invalid Region Type")
+		invalidRegionType(self)
 	end
 end
 
-function Region:GetPlayers()
+function Region:GetPlayersInRegion()
 	local players = {}
-
 	for _, char in ipairs(workspace.Players:GetChildren()) do
 		if self:PointInRegion(char:GetPivot().Position) then
 			table.insert(players, char)
 		end
 	end
-
 	return players
 end
 
