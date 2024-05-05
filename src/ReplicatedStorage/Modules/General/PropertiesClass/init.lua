a3506330b916a5778aaf0465a3f8288877104478
Windy15 <79671750+Windy15@users.noreply.github.com>
2024-05-04 20:45:50 -0400

    Fixed StatModifiers modifying order

--- src/ReplicatedStorage/Modules/General/PropertiesClass/init.lua
+++ src/ReplicatedStorage/Modules/General/PropertiesClass/init.lua
@@ -8,34 +8,33 @@ Properties.__index = Properties
 
 function Properties.new(new)
 	new = setmetatable(new or {}, Properties)
-
 	new.PropertyChanged = Signal.new()
 	new.StatModifiers = {}
-
 	return new
 end
 
 function Properties:GetModified(index)
 	local value = self[index]
-
 	if not value then return nil end
-
 	assert(self.StatModifiers[index], "Index '"..index.."' is not a stat for this table")
 
-    local muls = table.create(#self.StatModifiers[index])
+    local adders = table.create(#self.StatModifiers[index])
     local funcs = table.create(#self.StatModifiers[index])
 
 	for _, modifier in ipairs(self.StatModifiers[index]) do
-		if modifier.Adder then
-			value += modifier.Adder
+		if modifier.Multiplier then -- Multipliers first
+			value *= modifier.Multiplier
 		end
-
-        if modifier.Multiplier then table.insert(muls, modifier.Multiplier) end
-        if modifier.ModifierFunction then table.insert(funcs, modifier.ModifierFunction) end
+        if modifier.Adder then
+			table.insert(adders, modifier.Adder) -- Then adders
+		end
+        if modifier.ModifierFunction then
+			table.insert(funcs, modifier.ModifierFunction)-- Then functions
+		end
 	end
 
-	for _, mul in ipairs(muls) do
-		value *= mul
+	for _, add in ipairs(adders) do
+		value += add
 	end
 
     for _, func in ipairs(funcs) do
@@ -45,7 +44,7 @@ function Properties:GetModified(index)
 	return value
 end
 
-function Properties:SetStat(index)
+function Properties:CreateStat(index)
 	self.StatModifiers[index] = {
         -- StatModifier.new()
     }
