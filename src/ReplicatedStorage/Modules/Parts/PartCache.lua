bd9622490f93c795a31327327596874d76ba303b
Windy15 <79671750+Windy15@users.noreply.github.com>
2024-05-19 00:26:03 -0400

    Fixed PartCache cache removal and typechecking

--- src/ReplicatedStorage/Modules/Parts/PartCache.lua
+++ src/ReplicatedStorage/Modules/Parts/PartCache.lua
@@ -2,48 +2,77 @@
 --!native
 
 local CACHE_CFRAME = CFrame.new(1e6, 1e6, 1e6)
+local CACHE_TAG = "CACHED_PARTCACHE"
 
 export type PartCache = {
-    CreatePool: (self: PartCache, poolKey: any, part: Part?) -> (),
+    CreatePool: (self: PartCache, poolKey: any, part: BasePart?) -> CachePool,
+    IsCached: (self: PartCache, part: BasePart) -> boolean,
     [any]: CachePool
 }
 
 type CachePoolImpl = {
-    StorePart: (self: CachePool, part: Part) -> (),
-    GetPart: (self: CachePool) -> (),
+    StorePart: (self: CachePool, part: BasePart) -> (),
+    GetPart: (self: CachePool) -> BasePart,
+    ClearParts: (self: CachePool) -> (),
 }
 
 export type CachePool = typeof(setmetatable({} :: {
-    Part: Part,
-    [number]: Part
+    Part: BasePart,
+    [number]: BasePart
 }, {} :: CachePoolImpl))
 
-local PartCache= {} :: PartCache
+local PartCache = {} :: PartCache
 local CachePool = {} :: CachePoolImpl
 
-function PartCache:CreatePool(poolKey: any, part: Part?)
+function PartCache:CreatePool(poolKey: any, part: BasePart?)
     if PartCache[poolKey] then
         error(`CachePool '{poolKey}' already exists`, 2)
     end
-    PartCache[poolKey] = setmetatable({
-        Part = (typeof(poolKey) == "Instance" and poolKey or part) :: Part
+
+    if not part then
+        if typeof(poolKey) == "Instance" then
+            part = poolKey :: BasePart
+        else
+            error("poolKey or part arguement must be a BasePart", 2)
+        end
+    end
+
+    local pool = setmetatable({
+        Part = part
     }, CachePool)
+    PartCache[poolKey] = pool :: CachePool
+
+    return pool
 end
 
-function CachePool:StorePart(part: Part)
+function PartCache:IsCached(part: BasePart)
+    return part:HasTag(CACHE_TAG)
+end
+
+function CachePool:StorePart(part: BasePart)
     part.CFrame = CACHE_CFRAME
+    part:AddTag(CACHE_TAG)
     table.insert(self, part)
 end
 
 function CachePool:GetPart()
-    local part = self[#self]
+    local len = #self
+    local part: BasePart = self[len]
     if part then
-        part = part:Clone()
-        self[#self] = nil
+        part:RemoveTag(CACHE_TAG)
+        self[len] = nil
     else
         return self.Part:Clone()
     end
     return part
+end
+
+function CachePool:ClearParts()
+    for i, part in ipairs(self) do
+        part:RemoveTag(CACHE_TAG)
+        part:Destroy()
+        self[i] = nil
+    end
 end
 
 return PartCache
